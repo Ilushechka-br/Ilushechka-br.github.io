c2cfd1eeb3c4abbb99b36036da50c98b355e41d3
Ilushechka-br <122828998+Ilushechka-br@users.noreply.github.com>
2023-02-22 20:32:47 +0000

    o

--- scripticus.js
+++ scripticus.js
@@ -1,5 +1,11 @@
 window.onscroll = function() {
+    var scro = document.getElementById("src");
     if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
       console.log("Кінець сторінки");
+      scro.style.display="block";
+      scro.style.fontSize="1200 px";
+    }else{
+      scro.style.display="none";
+      scro.style.fontSize="1200";
     }
   };
